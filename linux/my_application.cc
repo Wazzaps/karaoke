468d2bd5332f1c6d5b2f27ff35a294c0f4cb1a37
David Shlemayev <secondlifedvi@gmail.com>
2020-07-13 10:57:33 +0300

    Work around flutter/flutter#59960 (Nvidia support)

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -16,11 +16,13 @@ static void my_application_activate(GApplication *application)
 {
   GtkWindow *window =
       GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
-  GtkHeaderBar *header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
-  gtk_widget_show(GTK_WIDGET(header_bar));
-  gtk_header_bar_set_title(header_bar, "Karaoke");
-  gtk_header_bar_set_show_close_button(header_bar, TRUE);
-  gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
+  // This works around flutter/flutter#59960
+  // GtkHeaderBar *header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
+  // gtk_widget_show(GTK_WIDGET(header_bar));
+  // gtk_header_bar_set_title(header_bar, "Karaoke");
+  // gtk_header_bar_set_show_close_button(header_bar, TRUE);
+  // gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
+  gtk_window_set_title(window, "Karaoke");
   gtk_window_set_default_size(window, 1280, 720);
   gtk_widget_show(GTK_WIDGET(window));
 
